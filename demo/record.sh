966a3625fc90341ee0ad8f750ad21a51b87564e8
Max Blomstervall <max.blomstervall@gmail.com>
2026-04-07 22:22:14 +0200

    chore(demo): drop nested nix-shell call from record.sh

--- demo/record.sh
+++ demo/record.sh
@@ -6,7 +6,7 @@ DEMO="$(cd "$(dirname "$0")" && pwd)"
 # cd into fixtures so "kanban demo.json" works (not "kanban demo/fixtures/demo.json")
 cd "$DEMO/fixtures"
 
-nix-shell ../shell.nix --run "vhs ../demo.tape"
+vhs ../demo.tape
 
 # Move outputs to demo directory
 mv demo.gif "$DEMO/demo.gif"
